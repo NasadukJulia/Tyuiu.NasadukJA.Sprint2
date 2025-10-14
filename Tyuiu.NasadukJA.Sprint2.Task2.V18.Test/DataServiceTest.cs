ce5dbfe21fd6d7f1ad6c955c435e5b9d863f7464
NasadukJulia <231513871+NasadukJulia@users.noreply.github.com>
2025-10-14 23:45:43 +0500

    Task2

--- Tyuiu.NasadukJA.Sprint2.Task2.V18.Test/DataServiceTest.cs
+++ Tyuiu.NasadukJA.Sprint2.Task2.V18.Test/DataServiceTest.cs
@@ -1,11 +1,21 @@
-﻿namespace Tyuiu.NasadukJA.Sprint2.Task2.V18.Test
+﻿using Tyuiu.NasadukJA.Sprint2.Task2.V18.Lib;
+
+namespace Tyuiu.NasadukJA.Sprint2.Task2.V18.Test
 {
     [TestClass]
     public sealed class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidCheckDotInShadedArea()
         {
+            DataService ds = new DataService();
+            int x = 3;
+            int y = 3;
+
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+
+            Assert.AreEqual(wait, res);
         }
     }
 }
--- Tyuiu.NasadukJA.Sprint2.Task2.V18/Program.cs
+++ Tyuiu.NasadukJA.Sprint2.Task2.V18/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tyuiu.NasadukJA.Sprint2.Task2.V18.Lib;
+
+// See https://aka.ms/new-console-template for more information
+DataService ds = new DataService();
+
+Console.Title = "Спринт #2 | Выполнила: Насадюк Ю. А. | СМАРТб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #2                                                               *");
+Console.WriteLine("* Тема: Оператор if - полная и краткая форма записи                       *");
+Console.WriteLine("* Задание #2                                                              *");
+Console.WriteLine("* Вариант #18                                                             *");
+Console.WriteLine("* Выполнила: Насадюк Юлия Андреевна | СМАРТб-25-1                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу на, которая запрашивает целые значения с клавиатуры  *");
+Console.WriteLine("* и вычисляет находится ли точка с координатами X,Y в                     *");
+Console.WriteLine("* заштрихованной области.                                                 *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Введите значение переменной X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение переменной Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+bool res = ds.CheckDotInShadedArea(x, y);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+if (res)
+{
+    Console.WriteLine("Точка находится в заштрихованной области");
+}
+else
+{
+    Console.WriteLine("Точка не находится в заштрихованной области");
+}
+
+Console.ReadKey();
