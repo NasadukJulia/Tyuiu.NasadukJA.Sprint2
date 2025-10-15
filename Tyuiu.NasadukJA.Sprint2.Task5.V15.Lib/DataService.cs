4b15d81d622299f40f97c88d8dfc003494608f7c
NasadukJulia <231513871+NasadukJulia@users.noreply.github.com>
2025-10-15 20:23:31 +0500

    Task5

--- Tyuiu.NasadukJA.Sprint2.Task5.V15.Lib/DataService.cs
+++ Tyuiu.NasadukJA.Sprint2.Task5.V15.Lib/DataService.cs
@@ -1,7 +1,46 @@
-﻿namespace Tyuiu.NasadukJA.Sprint2.Task5.V15.Lib
-{
-    public class DataService
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
 
+namespace Tyuiu.NasadukJA.Sprint2.Task5.V15.Lib
+{
+    public class DataService : ISprint2Task5V15
+    {
+        public string FindDayName(int k)
+        {
+            if (k < 1 || k > 365)
+            {
+                throw new ArgumentException($"День должен быть от 1 до 365. Значение {k}");
+            }
+            int day = k % 7;
+            string res;
+
+            switch (day)
+            {
+                case 1:
+                    res = "Понедельник";
+                    break;
+                case 2:
+                    res = "Вторник";
+                    break;
+                case 3:
+                    res = "Среда";
+                    break;
+                case 4:
+                    res = "Четверг";
+                    break;
+                case 5:
+                    res = "Пятница";
+                    break;
+                case 6:
+                    res = "Суббота";
+                    break;
+                case 0:
+                    res = "Воскресенье";
+                    break;
+                default:
+                    throw new ArgumentException($"Ошибка в вычислении дня недели");
+            }
+
+            return res;
+        }
     }
 }
--- Tyuiu.NasadukJA.Sprint2.Task5.V15/Program.cs
+++ Tyuiu.NasadukJA.Sprint2.Task5.V15/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tyuiu.NasadukJA.Sprint2.Task5.V15.Lib;
+
+// See https://aka.ms/new-console-template for more information
+DataService ds = new DataService();
+
+Console.Title = "Спринт #2 | Выполнила: Насадюк Ю. А. | СМАРТб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #2                                                               *");
+Console.WriteLine("* Тема: Оператор Switch                                                   *");
+Console.WriteLine("* Задание #5                                                              *");
+Console.WriteLine("* Вариант #15                                                             *");
+Console.WriteLine("* Выполнила: Насадюк Юлия Андреевна | СМАРТб-25-1                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая использует оператор switch, определяет,     *");
+Console.WriteLine("* каким днем недели является k-й день не високосного года, в котором      *");
+Console.WriteLine("* 1 января понедельник.                                                   *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Введите номер дня: ");
+int numDay = Convert.ToInt32(Console.ReadLine());
+
+string res;
+
+if ((numDay < 1) || (numDay > 365))
+{
+    res = "Введено неверное значение!";
+}
+else
+{
+    res = "День недели : " + ds.FindDayName(numDay);
+}
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(res);
+
+Console.ReadKey();
