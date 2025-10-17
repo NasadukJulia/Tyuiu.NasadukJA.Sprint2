5c38c6170fe2951e4e52a52e78efc7c9df75f0af
NasadukJulia <231513871+NasadukJulia@users.noreply.github.com>
2025-10-17 18:27:18 +0500

    Task6

--- Tyuiu.NasadukJA.Sprint2.Task6.V15.Lib/DataService.cs
+++ Tyuiu.NasadukJA.Sprint2.Task6.V15.Lib/DataService.cs
@@ -1,7 +1,30 @@
-﻿namespace Tyuiu.NasadukJA.Sprint2.Task6.V15.Lib
-{
-    public class DataService
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
 
+namespace Tyuiu.NasadukJA.Sprint2.Task6.V15.Lib
+{
+    public class DataService : ISprint2Task6V15
+    {
+        public string FindDayName(int k)
+        {
+            if (k < 1 || k > 365)
+            {
+                throw new ArgumentException($"День должен быть от 1 до 365. Значение {k}");
+            }
+
+            int day = k % 7;
+
+            switch (day)
+            {
+                case 1: return "Понедельник";
+                case 2: return "Вторник";
+                case 3: return "Среда";
+                case 4: return "Четверг";
+                case 5: return "Пятница";
+                case 6: return "Суббота";
+                case 0: return "Воскресенье";
+                default:
+                    throw new ArgumentException("День должен быть от 1 до 365.");
+            }
+        }
     }
 }
--- Tyuiu.NasadukJA.Sprint2.Task6.V15/Program.cs
+++ Tyuiu.NasadukJA.Sprint2.Task6.V15/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tyuiu.NasadukJA.Sprint2.Task6.V15.Lib;
+
+// See https://aka.ms/new-console-template for more information
+DataService ds = new DataService();
+
+Console.Title = "Спринт #2 | Выполнила: Насадюк Ю. А. | СМАРТб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #2                                                               *");
+Console.WriteLine("* Тема: Получение результата из Switch                                    *");
+Console.WriteLine("* Задание #6                                                              *");
+Console.WriteLine("* Вариант #15                                                             *");
+Console.WriteLine("* Выполнила: Насадюк Юлия Андреевна | СМАРТб-25-1                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Написать программу, которая использует сокращенную форму записи         *");
+Console.WriteLine("* оператора switch, определяет, каким днем недели является k-й день не    *");
+Console.WriteLine("* високосного года, в котором 1 января понедельник.                       *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Введите номер дня: ");
+int numDay = Convert.ToInt32(Console.ReadLine());
+
+string res;
+
+if ((numDay < 1) || (numDay > 365))
+{
+    res = "Введено неверное значение!";
+}
+else
+{
+    res = "День недели : " + ds.FindDayName(numDay);
+}
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(res);
+
+Console.ReadKey();
